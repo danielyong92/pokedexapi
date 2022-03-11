f9aa343fd255b9b4e4001b6831850412ac2b943a
hz298786@hertz.net <hz298786@hertz.net>
2022-03-11 14:13:58 -0600

    added logic to populate carousel pokemon stats on click

--- javascript/index.js
+++ javascript/index.js
@@ -11,57 +11,110 @@ let pokemonType = document.getElementById("pokemon-type");
 let pokemonOrder = document.getElementById("pokemon-order");
 
 let pokemonData;
+let frontPokemonImage;
+let backPokemonImage;
+let currentPokemonImage;
 
+let pokemon;
 const pokemonSearch = async term => {
   const url = `https://pokeapi.co/api/v2/pokemon/${term}`;
   const response = await fetch(url);
 
-  const pokemon = await response.json();
+  pokemon = await response.json();
   pokemonData = pokemon;
-//   console.log("pokemon", pokemon);
+  frontPokemonImage = pokemon.sprites.front_default;
+  backPokemonImage = pokemon.sprites.back_default;
+  currentPokemonImage = frontPokemonImage;
+  console.log("pokemon", pokemon);
 
   // document.getElementById('update_image').setAttribute('src', pokemon.sprites.other.dream_world.front_default);
   saveButton.style.display = "block";
-  pokemonImage.style.display="block";
+  pokemonImage.style.display = "block";
 
   document
     .getElementById("update_image")
-    .setAttribute("src", pokemon.sprites.front_default);
+    .setAttribute("src", currentPokemonImage);
   document.getElementById("pokemon-name").innerHTML = `Name: ${pokemon.name}`;
   pokemonHeight.innerHTML = pokemon.height;
   pokemonWeight.innerHTML = pokemon.weight;
   pokemonOrder.innerHTML = pokemon.order;
 
-  let array = pokemon.types
-  array.forEach( x => {
-      pokemonType.innerHTML += `${x.type.name} `});
+  let array = pokemon.types;
+  array.forEach(x => {
+    pokemonType.innerHTML = x.type.name;
+  });
+
+  pokemonImage.onclick = () => {
+    if (currentPokemonImage === frontPokemonImage) {
+      pokemonStats.style.display = "block";
+      pokemonImage.setAttribute("src", backPokemonImage);
+      currentPokemonImage = backPokemonImage;
+    } else {
+      document
+        .getElementById("update_image")
+        .setAttribute("src", frontPokemonImage);
+      currentPokemonImage = frontPokemonImage;
+      pokemonStats.style.display = "none";
+    }
+  };
 };
 
-const appendPokemon = pokemonData => {
+// const onClickPopulate = () => {
+//     document
+//       .getElementById("update_image")
+//       .setAttribute("src", pokemonData.sprites.front_default);
+//     document.getElementById(
+//       "pokemon-name"
+//     ).innerHTML = `Name: ${pokemonData.name}`;
+//     pokemonHeight.innerHTML = pokemonData.height;
+//     pokemonWeight.innerHTML = pokemonData.weight;
+//     pokemonOrder.innerHTML = pokemonData.order;
+//     let array = pokemonData.types;
+//     array.forEach(x => {
+//       pokemonType.innerHTML = x.type.name;
+//     });
+// }
+
+const appendPokemon = pokemon => {
   let carouselDiv = document.getElementById("carousel");
   let images = document.createElement("img");
-  images.setAttribute("src", pokemonData.sprites.front_default);
+  images.setAttribute("src", pokemon.sprites.front_default);
   images.setAttribute("height", 150);
   images.setAttribute("width", 150);
   carouselDiv.appendChild(images);
+
+  images.onclick = () => {
+    document
+      .getElementById("update_image")
+      .setAttribute("src", pokemon.sprites.front_default);
+    frontPokemonImage = pokemon.sprites.front_default;
+    backPokemonImage = pokemon.sprites.back_default;
+    currentPokemonImage = frontPokemonImage;
+    document.getElementById("pokemon-name").innerHTML = `Name: ${pokemon.name}`;
+    pokemonHeight.innerHTML = pokemon.height;
+    pokemonWeight.innerHTML = pokemon.weight;
+    pokemonOrder.innerHTML = pokemon.order;
+    let array = pokemon.types;
+    array.forEach(x => {
+      pokemonType.innerHTML = x.type.name;
+    });
+  };
 };
 
-const flipPokemon = pokemonData => {
-  let imageSrc = document.getElementById("update_image").src;
-  const backside = pokemonData.sprites.back_default;
-  const frontside = pokemonData.sprites.front_default;
+// const flipPokemon = pokemonData => {
+//   let imageSrc = document.getElementById("update_image").src;
+//   const backside = pokemonData.sprites.back_default;
+//   const frontside = pokemonData.sprites.front_default;
 
-  if (imageSrc == frontside) {
-    pokemonStats.style.display = "block";
-    pokemonImage.setAttribute("src", backside);
-  } else {
-    document.getElementById("update_image").setAttribute("src", frontside);
-    pokemonStats.style.display = "none";
-  }
-};
+//   if (imageSrc === frontside) {
+//     pokemonStats.style.display = "block";
+//     pokemonImage.setAttribute("src", backside);
+//   } else {
+//     document.getElementById("update_image").setAttribute("src", frontside);
+//     pokemonStats.style.display = "none";
+//   }
+// };
 
 searchButton.addEventListener("click", () => pokemonSearch(pokemonName.value));
-saveButton.addEventListener("click", () =>
-  appendPokemon(pokemonData)
-);
-pokemonImage.addEventListener("click", () => flipPokemon(pokemonData));
+saveButton.addEventListener("click", () => appendPokemon(pokemon));
+// pokemonImage.addEventListener("click", () => flipPokemon(pokemonData));
