b40d8e81178f48079e0517cf14a437e72722ba74
hz298786@hertz.net <hz298786@hertz.net>
2022-03-10 22:52:00 -0600

    added css and also flip logic and carousel appendchild logic

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,45 @@
 const pokemonName = document.getElementById("pokemonName");
 const searchButton = document.getElementById("search_btn");
+const saveButton = document.getElementById("save_btn");
+const pokemonImage = document.getElementById("update_image");
 
+let pokemonData;
 const pokemonSearch = async term => {
     const url = `https://pokeapi.co/api/v2/pokemon/${term}`;
     const response = await fetch(url);
     
-    if( response.status == 404 || response.statusText == "Not Found") {
-        console.log("not found");
-        document.getElementById("show_error").classList.add('show');
-        document.getElementById("show_error").classList.remove("hidden");
-        return
-    }
-
     const pokemon = await response.json();
     console.log("pokemon", pokemon);
 
-    document.getElementById('update_image').setAttribute('src', pokemon.sprites.other.dream_world.front_default);
+    // document.getElementById('update_image').setAttribute('src', pokemon.sprites.other.dream_world.front_default);
+    document.getElementById('update_image').setAttribute('src', pokemon.sprites.front_default);
+    document.getElementById('pokemon-name').innerHTML = `Name: ${pokemon.name}`;
+    pokemonData = pokemon;
+}
 
+const appendPokemon = (pokemonData) => {
+    let carouselDiv = document.getElementById("carousel");
+    let images = document.createElement("img");
+    images.setAttribute('src', pokemonData);
+    carouselDiv.appendChild(images);
+}
+
+const flipPokemon = (pokemonData) => {
+    let imageSrc = document.getElementById("update_image").src;
+    const backside = pokemonData.sprites.back_default;
+    const frontside = pokemonData.sprites.front_default;
+
+    if (imageSrc == pokemonData.sprites.front_default) {
+        document.getElementById('update_image').setAttribute('src', backside);
+    }
+    else {
+        document.getElementById('update_image').setAttribute('src', frontside);
+    }
 }
 
 searchButton.addEventListener('click', () => pokemonSearch(pokemonName.value));
+saveButton.addEventListener('click', () => appendPokemon(pokemonData.sprites.front_default));
+pokemonImage.addEventListener('click', ()=> flipPokemon(pokemonData));
+
+    
+
