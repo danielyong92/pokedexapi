2d5f2c99934803a02bf6302918feb471a9eed249
hz298786@hertz.net <hz298786@hertz.net>
2022-03-11 00:25:50 -0600

    added more pokemon stats and array loop to pokemon type

--- javascript/index.js
+++ javascript/index.js
@@ -1,45 +1,67 @@
 const pokemonName = document.getElementById("pokemonName");
 const searchButton = document.getElementById("search_btn");
 const saveButton = document.getElementById("save_btn");
-const pokemonImage = document.getElementById("update_image");
+let pokemonImage = document.getElementById("update_image");
+
+//Pokemon Stats
+let pokemonStats = document.getElementById("pokemon-stats");
+let pokemonHeight = document.getElementById("pokemon-height");
+let pokemonWeight = document.getElementById("pokemon-weight");
+let pokemonType = document.getElementById("pokemon-type");
+let pokemonOrder = document.getElementById("pokemon-order");
 
 let pokemonData;
+
 const pokemonSearch = async term => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${term}`;
-    const response = await fetch(url);
-    
-    const pokemon = await response.json();
-    console.log("pokemon", pokemon);
+  const url = `https://pokeapi.co/api/v2/pokemon/${term}`;
+  const response = await fetch(url);
 
-    // document.getElementById('update_image').setAttribute('src', pokemon.sprites.other.dream_world.front_default);
-    document.getElementById('update_image').setAttribute('src', pokemon.sprites.front_default);
-    document.getElementById('pokemon-name').innerHTML = `Name: ${pokemon.name}`;
-    pokemonData = pokemon;
-}
+  const pokemon = await response.json();
+  pokemonData = pokemon;
+  console.log("pokemon", pokemon);
 
-const appendPokemon = (pokemonData) => {
-    let carouselDiv = document.getElementById("carousel");
-    let images = document.createElement("img");
-    images.setAttribute('src', pokemonData);
-    carouselDiv.appendChild(images);
-}
+  // document.getElementById('update_image').setAttribute('src', pokemon.sprites.other.dream_world.front_default);
+  saveButton.style.display = "block";
+  pokemonImage.style.display="block";
 
-const flipPokemon = (pokemonData) => {
-    let imageSrc = document.getElementById("update_image").src;
-    const backside = pokemonData.sprites.back_default;
-    const frontside = pokemonData.sprites.front_default;
+  document
+    .getElementById("update_image")
+    .setAttribute("src", pokemon.sprites.front_default);
+  document.getElementById("pokemon-name").innerHTML = `Name: ${pokemon.name}`;
+  pokemonHeight.innerHTML = pokemon.height;
+  pokemonWeight.innerHTML = pokemon.weight;
+  pokemonOrder.innerHTML = pokemon.order;
 
-    if (imageSrc == pokemonData.sprites.front_default) {
-        document.getElementById('update_image').setAttribute('src', backside);
-    }
-    else {
-        document.getElementById('update_image').setAttribute('src', frontside);
-    }
-}
+  let array = pokemon.types
+  array.forEach( x => {
+      console.log("name", x.type.name);
+      pokemonType.innerHTML += `${x.type.name} `});
+  
+};
 
-searchButton.addEventListener('click', () => pokemonSearch(pokemonName.value));
-saveButton.addEventListener('click', () => appendPokemon(pokemonData.sprites.front_default));
-pokemonImage.addEventListener('click', ()=> flipPokemon(pokemonData));
+const appendPokemon = pokemonData => {
+  let carouselDiv = document.getElementById("carousel");
+  let images = document.createElement("img");
+  images.setAttribute("src", pokemonData);
+  carouselDiv.appendChild(images);
+};
 
-    
+const flipPokemon = pokemonData => {
+  let imageSrc = document.getElementById("update_image").src;
+  const backside = pokemonData.sprites.back_default;
+  const frontside = pokemonData.sprites.front_default;
 
+  if (imageSrc == frontside) {
+    pokemonStats.style.display = "block";
+    pokemonImage.setAttribute("src", backside);
+  } else {
+    document.getElementById("update_image").setAttribute("src", frontside);
+    pokemonStats.style.display = "none";
+  }
+};
+
+searchButton.addEventListener("click", () => pokemonSearch(pokemonName.value));
+saveButton.addEventListener("click", () =>
+  appendPokemon(pokemonData.sprites.front_default)
+);
+pokemonImage.addEventListener("click", () => flipPokemon(pokemonData));
